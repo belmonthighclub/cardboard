54466d8341a0752b6e2db1f702170fd52ac9799f
Bagel the Bagel <92469016+bagel2255@users.noreply.github.com>
2021-11-10 14:32:35 -0500

    routingWorks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
-import { AppRoutingModule } from './app-routing.module';
 import { InformationComponent } from './information/information.component';
 
 @NgModule({
@@ -14,6 +15,7 @@ import { InformationComponent } from './information/information.component';
   ],
   imports: [
     BrowserModule,
+    RouterModule,
     AppRoutingModule
   ],
   providers: [],
