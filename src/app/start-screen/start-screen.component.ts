453b5fa02ddfbb9730a966219c049b05101db7a9
rgarg23 <rit.g.garg@gmail.com>
2021-12-19 15:01:29 -0500

    Added all of start-screen.html and most of start-screen.ts (look at commit to see exactly what I did)

--- src/app/start-screen/start-screen.component.ts
+++ src/app/start-screen/start-screen.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { interval, Subscription } from 'rxjs';
 
 const LOOPS_PER_SECOND = 1;
@@ -12,19 +12,40 @@ const LOOPS_PER_SECOND = 1;
 export class StartScreenComponent implements OnInit, OnDestroy {
   private cellsPerRow: number = 7;
   private mines: number = 7;
-  private playing: boolean = true;
+  private playing: boolean = false;
+  private victory: boolean | null = null;
   private subscription: Subscription = new Subscription(); //used to loop a method
   
   public getPlaying(): boolean { //getter for start
     return this.playing;
+  }
+
+  public getVictory(): boolean | null {
+    return this.victory;
   }
   
   public getCellsPerRow(): number { //getter for cellsPerRow
     return this.cellsPerRow;
   }
   
+  public setCellsPerRow(newCellsPerRow: number): void { //setter for cellsPerRow
+    this.cellsPerRow = newCellsPerRow;
+  }
+
+  public setCellsPerRowEvent(event: Event): void { //setter for cellsPerRow (event)
+    this.cellsPerRow = Number((event.target as HTMLInputElement).value);
+  }
+
   public getMines(): number { //getter for mines
     return this.mines;
+  }
+
+  public setMines(newMines: number): void { //setter for mines
+    this.mines = newMines;
+  }
+
+  public setMinesEvent(event: Event): void { //setter for mines (event)
+    this.mines = Number((event.target as HTMLInputElement).value);
   }
 
   constructor() {
@@ -41,5 +62,21 @@ export class StartScreenComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subscription && this.subscription.unsubscribe();
+  }
+
+  public processEmit(event: number): void {
+    if (event != 0) {
+      this.playing = false;
+    }
+    if (event == 1) {
+      this.victory = true;
+    }
+    else if (event == -1) {
+      this.victory = false;
+    }
+  }
+
+  public submission(): void {
+    this.playing = true;
   }
 }
